94e584d39b20c9b4e4002815ef9afdc3b496081b
Pragya121 <pragya@podium.school>
2022-10-18 11:52:11 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import Loading from "./components/Loading";
+
+import data from "./assets/data";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Signin from "./components/Signin";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isSignedout, setIsSignedout] = React.useState(false);
+  // const [user, setUser] = React.useState(
+  //   localStorage.getItem("user")
+  //     ? JSON.parse(localStorage.getItem("token"))
+  //     : {}
+  // );
+  const [para, setPara] = React.useState("Loading...");
+  // const role = user ? user.role : null;
+  const [token, setToken] = React.useState(
+    localStorage.getItem("token")
+      ? JSON.parse(localStorage.getItem("token"))
+      : null
   );
+
+  const add = window.location.href;
+  var base_url = window.location.origin;
+
+  var host = window.location.host;
+
+  var pathEx = add.replace(base_url, "");
+  console.log("address", pathEx);
+  const getLink = async () => {
+    setIsLoading(true);
+
+    try {
+      let config = {
+        params: {
+          path: pathEx,
+        },
+      };
+
+      const response = await data.axios.get(
+        `${data.BASE_URL}/qrRedirection`,
+
+        config
+      );
+      const resp = response.data;
+
+      console.log(`info`, resp);
+      setPara(resp);
+      debugger;
+      window.location.replace("https://" + resp);
+
+      setIsLoading(false);
+
+      return resp;
+    } catch (errors) {
+      setIsLoading(false);
+      console.error(errors);
+      if (errors.response.status) {
+        setPara("");
+      } else {
+        alert(errors.response.data.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getLink();
+    console.log("Script loaded");
+  }, []);
+
+  if (!isLoading) {
+    return (
+      <div className="App">
+        <Navbar />
+
+        <div className="body">
+          {" "}
+          <div id="editor">
+            {para ? <>Please wait...</> : <> 404 Not found</>}
+          </div>
+          <p></p>
+          {/* <div
+              onClick={(e) => {
+                const editor = document.getElementById("editor");
+                Sapling.observe(editor);
+              }}
+            >
+               
+            </div> */}
+          {/* <button
+              onClick={(e) => {
+                const editor = document.getElementById("editor");
+                Sapling.unobserve(editor);
+              }}
+            >
+             Stop
+            </button> */}
+        </div>
+        {/* <Footer /> */}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  }
 }
 
 export default App;
